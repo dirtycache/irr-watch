9bdaff555db634a074245087a5b2105e9a864b9b
MIDnet Engineering <eng@westie.mid.net>
2021-11-03 18:20:35 -0500

    IRR changes

--- do-watch.sh
+++ do-watch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-cd /home/eng/irr-watch
+
 MAINTAINER=`cat maintainers.txt | sed "s/ *#.*$//g" | grep .`
 IRRDB=`cat registries.txt | sed "s/ *#.*$//g" | grep .`
 WHOIS=`which whois`
 GIT=`which git`
 TIMESTAMP=`printf '%(%Y-%m-%d.%H%M%S)T\n' -1`
 
+cd /home/eng/irr-watch
 /usr/bin/rm -f autnum_*.txt
 
 while read IRR; do
@@ -17,6 +18,9 @@ done <<< $IRRDB
 while read IRR; do
         while read MNT; do
                 $WHOIS -h $IRR -i mnt-by $MNT | grep aut-num | awk '{print $2}' >> autnum_$IRR.txt
+		while read AUTNUM; do
+			$WHOIS -h $IRR -i origin $AUTNUM > routes-origin-$AUTNUM-$IRR.txt 
+		done <<< `cat autnum_$IRR.txt`
         done <<< $MAINTAINER
 done <<< $IRRDB
 
